e6852d5a8d13ef695abdb7c33aafa545784c7a2d
Pierre W <rr21multi@gmail.com>
2024-04-02 17:06:47 +0200

    début de classe Vecteur2D

---
+++ code/vecteur_2d.py
@@ -0,0 +1,25 @@
+class Vecteur2D:
+    def __init__(self, x:float, y:float):
+        self.x = x
+        self.y = y
+    
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
+    
+    def __add__(self, rhs):
+        return Vecteur2D(self.x + rhs.x, self.y + rhs.y)
+    
+    def __sub__(self, rhs):
+        return Vecteur2D(self.x - rhs.x, self.y - rhs.y)
+    
+    def scalaire(self, rhs):
+        return Vecteur2D(self.x * rhs.x, self.y * rhs.y)
+
+    def __mul__(self, rhs):
+        if type(rhs) == Vecteur2D:
+            return self.scalaire(rhs)
+        else:
+            return Vecteur2D(self.x * rhs, self.y * rhs)

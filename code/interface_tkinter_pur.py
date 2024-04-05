03a741ad9f08e6e801a657d1f4c3b5760825f42e
Pierre W <rr21multi@gmail.com>
2024-04-06 00:12:50 +0200

    début de création de carte avec vérifications de possibilité locales au curseur

--- code/interface_tkinter_pur.py
+++ code/interface_tkinter_pur.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 import os
 from os.path import abspath, dirname
 import math
-#import numpy as np
+import numpy as np
 
 def from_rgb(rgb):
     """prend un tuple rgb et le transforme en string héxadécimal de couleur tkinter
@@ -17,6 +17,11 @@ def from_rgb(rgb):
     """
     return "#%02x%02x%02x" % rgb
 
+BLANC = from_rgb((255,255,255))
+NOIR = from_rgb((0,0,0))
+ROUGE = from_rgb((255,0,0))
+VERT = from_rgb((0,255,0))
+
 class Fenetre(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -25,8 +30,8 @@ class Fenetre(tk.Tk):
         self.resizable(True, True)
         self.title('Interface de test tkinter')
 
-        self.largeur_carte = 20
-        self.hauteur_carte = 20
+        self.largeur_carte = 25
+        self.hauteur_carte = 25
         self.echelle = 16 # Nombre de pixels de coté d'une case sur la grille
 
         self.columnconfigure(0, weight=1, uniform='a')
@@ -84,43 +89,67 @@ class Fenetre(tk.Tk):
         self.slider_agressivite = tk.Scale(self.frame_sliders, from_=1, to=100, orient="horizontal")
         self.slider_agressivite.grid(row=1, column=1, sticky='nsew')
 
+        self.frame_sliders.pack(side=tk.LEFT, fill="y")
+        self.frame_param_simu.grid(column=0, row=1, columnspan=2, sticky='nsew')
+        self.frame_param_carte.grid(column=0, row=0, sticky='nsew')
+        self.setup_frame_canvas()
+        
+
+        self.previsualisation = False
+
+        self.after(1000, self.affiche_carte_dans_canvas)
+        self.after(50, self.surligne_case_selectionnee)
+
+    def setup_frame_canvas(self):
+        self.frame_canvas_affichage = tk.Frame(self,highlightbackground="black", highlightthickness=3)
+        self.label_affichage = tk.Label(self.frame_canvas_affichage, text="Affichage de la carte et de la simulation", font="Calibri 16 bold")
+        self.label_affichage.pack(side=tk.TOP, fill="x")
         self.largeur_canvas = 100
         self.hauteur_canvas = 100
         self.xo = 0
         self.yo = 0
         self.last_mouse_coords = None
         self.grille_canvas = []
-
-        self.frame_sliders.pack(side=tk.LEFT, fill="y")
-        self.frame_param_simu.grid(column=0, row=1, columnspan=2, sticky='nsew')
-        self.frame_param_carte.grid(column=0, row=0, sticky='nsew')
-
-        self.frame_canvas_affichage = tk.Frame(self,highlightbackground="black", highlightthickness=3)
-        self.label_affichage = tk.Label(self.frame_canvas_affichage, text="Affichage de la carte et de la simulation", font="Calibri 16 bold")
-        self.label_affichage.pack(side=tk.TOP, fill="x")
+        self.grille_route = np.zeros((self.largeur_carte, self.hauteur_carte))
+        self.routes_placees = 0
         self.canvas_affichage = tk.Canvas(self.frame_canvas_affichage, background="green")
-        self.canvas_affichage.bind("<Configure>", self.setup_canvas)
+        self.canvas_affichage.bind("<Configure>", self.affiche_carte_dans_canvas)
+        self.canvas_affichage.bind("<B1-Motion>", self.rajoute_route_click)
+        self.canvas_affichage.bind("<B3-Motion>", self.enleve_route_click)
         self.canvas_affichage.pack(fill="both", expand=True)
         self.frame_canvas_affichage.grid(column=1, row=0, sticky='nsew')
 
-        self.previsualisation = False
-
-        self.after(1000, self.setup_canvas)
-        self.after(50, self.surligne_case_selectionnee)
+    def rajoute_route_click(self, event=None):
+        coords = self.calcul_coordonnees_souris_carte()
+        if coords != None:
+            xc, yc = coords
+            if self.position_posable(xc, yc):
+                if self.grille_route[xc,yc] == 0:
+                    self.routes_placees += 1
+                self.grille_route[xc,yc] = 1
+                self.canvas_affichage.itemconfigure(self.grille_canvas[yc * self.hauteur_carte + xc], fill=self.couleur_de_case(xc, yc))
+    def enleve_route_click(self, event=None):
+        coords = self.calcul_coordonnees_souris_carte()
+        if coords != None:
+            xc, yc = coords
+            if self.grille_route[xc,yc] == 1:
+                self.routes_placees -= 1
+            self.grille_route[xc,yc] = 0
+            self.canvas_affichage.itemconfigure(self.grille_canvas[yc * self.hauteur_carte + xc], fill=self.couleur_de_case(xc, yc))
 
     def surligne_case_selectionnee(self):
         coords = self.calcul_coordonnees_souris_carte()
         if coords != None:
             xc, yc = coords
             if self.last_mouse_coords == None:
-                self.canvas_affichage.itemconfigure(self.grille_canvas[yc * self.hauteur_carte + xc], fill=from_rgb((255,0,0)))
+                self.canvas_affichage.itemconfigure(self.grille_canvas[yc * self.hauteur_carte + xc], fill=self.couleur_de_curseur(xc, yc))
             else:
                 lxc, lyc = self.last_mouse_coords
-                self.canvas_affichage.itemconfigure(self.grille_canvas[lyc * self.hauteur_carte + lxc], fill=from_rgb((255,255,255)))
-                self.canvas_affichage.itemconfigure(self.grille_canvas[yc * self.hauteur_carte + xc], fill=from_rgb((255,0,0)))
+                self.canvas_affichage.itemconfigure(self.grille_canvas[lyc * self.hauteur_carte + lxc], fill=self.couleur_de_case(lxc, lyc))
+                self.canvas_affichage.itemconfigure(self.grille_canvas[yc * self.hauteur_carte + xc], fill=self.couleur_de_curseur(xc, yc))
         elif self.last_mouse_coords != None:
             lxc, lyc = self.last_mouse_coords
-            self.canvas_affichage.itemconfigure(self.grille_canvas[lyc * self.hauteur_carte + lxc], fill=from_rgb((255,255,255)))
+            self.canvas_affichage.itemconfigure(self.grille_canvas[lyc * self.hauteur_carte + lxc], fill=self.couleur_de_case(lxc, lyc))
         self.last_mouse_coords = coords
         self.after(50, self.surligne_case_selectionnee)
 
@@ -134,7 +163,19 @@ class Fenetre(tk.Tk):
             ret = (xc, yc)
         return ret
 
-    def setup_canvas(self, event=None):
+    def couleur_de_case(self, xc, yc):
+        ret = BLANC
+        if self.grille_route[xc, yc] == 1:
+            ret = NOIR
+        return ret
+
+    def couleur_de_curseur(self, xc, yc):
+        ret = ROUGE
+        if self.position_posable(xc, yc):
+            ret = VERT
+        return ret
+
+    def affiche_carte_dans_canvas(self, event=None):
         if self.largeur_canvas != self.canvas_affichage.winfo_width() or self.hauteur_canvas != self.canvas_affichage.winfo_height():
             self.calcul_echelle()
             self.xo, self.yo = 0, 0
@@ -145,20 +186,44 @@ class Fenetre(tk.Tk):
             
             if len(self.grille_canvas) == 0:
                 self.canvas_affichage.delete('all')
+                nx = np.ones(self.largeur_carte+1) * self.xo + np.arange(0, self.largeur_carte+1) * self.echelle
+                ny = np.ones(self.hauteur_carte+1) * self.yo + np.arange(0, self.hauteur_carte+1) * self.echelle
                 for y in range(self.hauteur_carte):
                     for x in range(self.largeur_carte):
-                        self.grille_canvas.append(self.canvas_affichage.create_rectangle(self.xo + x*self.echelle, self.yo + y*self.echelle, self.xo + (x+1)*self.echelle, self.yo + (y+1)*self.echelle, fill=from_rgb((255,255,255)), outline=from_rgb((0,0,0))))
+                        self.grille_canvas.append(self.canvas_affichage.create_rectangle(nx[x], ny[y], nx[x+1], ny[y+1], fill=from_rgb((255,255,255)), outline=from_rgb((0,0,0))))
             elif 0 < len(self.grille_canvas) < self.largeur_carte * self.hauteur_carte:
                 pass
             else:
+                nx = np.ones(self.largeur_carte+1) * self.xo + np.arange(0, self.largeur_carte+1) * self.echelle
+                ny = np.ones(self.hauteur_carte+1) * self.yo + np.arange(0, self.hauteur_carte+1) * self.echelle
                 for y in range(self.hauteur_carte):
                     for x in range(self.largeur_carte):
                         rect = self.grille_canvas[y * self.largeur_carte + x]
-                        self.canvas_affichage.coords(rect, self.xo + x*self.echelle, self.yo + y*self.echelle, self.xo + (x+1)*self.echelle, self.yo + (y+1)*self.echelle)
+                        self.canvas_affichage.coords(rect, nx[x], ny[y], nx[x+1], ny[y+1])
                         #self.canvas_affichage.itemconfigure() 
 
+    def position_posable(self, xc, yc) -> bool:
+        positions_a_check_coins = [[(-1, -1), (-1, 0), (0, -1)], [(-1, 1), (-1, 0), (0, 1)], [(1,1), (0,1), (1,0)], [(1, -1), (0, -1), (1,0)]] # triplets de positions de coins
+        ret = True
+        for triplet in positions_a_check_coins:
+            compte = 0
+            for (dx, dy) in triplet:
+                compte += self.point_dans_grille_ou_0(xc + dx, yc + dy)
+            if compte == 3:
+                ret = False
 
-
+        positions_a_check_cotes = [(-1,0), (1,0), (0, 1), (0, -1)]
+        total_cotes = 0
+        for (dx, dy) in positions_a_check_cotes:
+            total_cotes += self.point_dans_grille_ou_0(xc + dx, yc + dy)
+        if total_cotes == 0 and self.routes_placees > 0:
+            ret = False
+        return ret
+    def point_dans_grille_ou_0(self, xc, yc) -> int:
+        ret = 0
+        if 0 <= xc < self.largeur_carte and 0 <= yc < self.hauteur_carte:
+            ret = self.grille_route[xc,yc]
+        return ret
     def calcul_echelle(self):
         self.largeur_canvas = self.canvas_affichage.winfo_width()
         self.hauteur_canvas = self.canvas_affichage.winfo_height()
@@ -183,7 +248,7 @@ class Fenetre(tk.Tk):
 
 
     def essaie_affiche(self, event=None):
-        if not self.previsualisation :
+        if not self.previsualisation and False: # LE AND FALSE EST JUSTE RAJOUTE POUR DESACTIVER LA FEATURE
             self.previsualisation = True
 
             route_choisie = self.liste_cartes.get("active")

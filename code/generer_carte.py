c8ba63fdeab5a8eceffc670f6eb5258fc25c61ce
Pyno <pyno.prog@gmail.com>
2024-04-07 22:59:19 +0200

    generator carte change

--- code/generer_carte.py
+++ code/generer_carte.py
@@ -1,13 +1,15 @@
 import random
 import numpy as np
 import os
+import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib.colors import ListedColormap
 
 class Carte:
 
     NOEUD = 1
-    ARRETE = 2    
+    ENTREE_SORTIE = 2    
+    ARRETE = 3
 
     def __init__(self, taille: list[int, int] = [10, 10], distance_entre_noeuds = 5) -> None:
         width, height = taille
@@ -30,6 +32,7 @@ class Carte:
         self.porbabilite_entree_sortie = 1/10
         self.nombre_entree_sortie = int((2*width + 2*height) * self.porbabilite_noeud) + 2
 
+
     def return_max_grille(self, grille) -> int:
         return max([max(ligne) for ligne in grille])
 
@@ -48,6 +51,32 @@ class Carte:
             print()
 
     def affiche_grille(self, grille: list[list[int]]):
+
+        # Create a figure and axis
+        fig, ax = plt.subplots()
+        # Create a colormap with white for empty spaces
+        # Create a custom colormap
+        cmap = ListedColormap(['white', 'red', 'blue', 'green'])
+
+        # Set the color for empty spaces to white
+        cmap.set_bad(color='white')
+
+        # Plot the grid as an image with the colormap
+        im = ax.imshow(grille, cmap=cmap, vmin=0, vmax=3)
+
+        # Add grid lines
+        ax.grid(True, which='both', color='black', linewidth=1)
+        ax.set_xticks(np.arange(0, self.width, 1))
+        ax.set_yticks(np.arange(0, self.height, 1))
+
+        # Create a colorbar legend
+        cbar = ax.figure.colorbar(im, ax=ax, ticks=[0, 1, 2, 3])
+        cbar.ax.set_yticklabels(['Empty', 'Node', 'Entry/Exit', 'Edge'])
+
+        # Show the plot
+        plt.show()
+
+    def affiche_grille_proba(self, grille: list[list[int]]):
         # Convert the grid values to a numpy array
         grid_array = np.array(grille)
 
@@ -187,10 +216,72 @@ class Carte:
         print(positions_noeuds_)
         print(self.nombre_noeud)"""
 
+    def generer_entree_sortie(self):
+        grille_probas = [[0 for _ in range(self.width)] for _ in range(self.height)]
+
+        perimetre = self.width*2 + self.height*2
+
+        proba_entree_sortie = self.nombre_entree_sortie / perimetre
+
+        for i in range(self.width):
+            grille_probas[0][i] = proba_entree_sortie
+            grille_probas[self.height-1][i] = proba_entree_sortie
+
+        for i in range(self.height):
+            grille_probas[i][0] = proba_entree_sortie
+            grille_probas[i][self.width-1] = proba_entree_sortie
+
+        liste_positions_noeuds = self.return_positions_de(self.NOEUD, self.grille)
+        for position in liste_positions_noeuds:
+            i, j = position
+            for x in range(i - self.distance_entre_noeud, i + self.distance_entre_noeud + 1):
+                for y in range(j - self.distance_entre_noeud, j + self.distance_entre_noeud + 1):
+                    if 0 <= x < self.height and 0 <= y < self.width:
+                        grille_probas[x][y] = 0
+
+
+        # Generate entry and exit points
+        positions_entree_sortie = []
+        while len(positions_entree_sortie) < self.nombre_entree_sortie:
+            positions_noeuds = self.return_positions_de_non(-1, grille_probas)
+            dict_proba = {}
+            for x, y in positions_noeuds:
+                dict_proba[str((x, y))] = grille_probas[x][y]
+
+            sum_proba = sum(dict_proba.values())
+
+            if sum_proba == 0:
+                break
+
+            for key in dict_proba.keys():
+                dict_proba[key] /= sum_proba
+
+            # Randomly select a position based on the probabilities
+            position = np.random.choice(a=list(dict_proba.keys()), p=list(dict_proba.values()))
+
+            del dict_proba[position]
+
+            position = [int(i) for i in position[1:-1].split(",")]
+            # Update the grid with the new entry/exit point
+            self.grille[position[0]][position[1]] = self.ENTREE_SORTIE
+
+            
+
+            positions_entree_sortie.append(position)
+
+
+            for x in range(position[0] - self.distance_entre_noeud, position[0] + self.distance_entre_noeud + 1):
+                for y in range(position[1] - self.distance_entre_noeud, position[1] + self.distance_entre_noeud + 1):
+                    if 0 <= x < self.height and 0 <= y < self.width:
+                        grille_probas[x][y] = 0
         
+
+        
+
         
 
 carte = Carte([20, 20], 5)
 carte.generer_noeud()
-carte.affiche_grille_terminal(carte.grille)
+carte.affiche_grille(carte.grille)
+carte.generer_entree_sortie()
 carte.affiche_grille(carte.grille)

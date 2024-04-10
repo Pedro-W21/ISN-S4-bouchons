836903006da0d149753dca31112519fff1a5b441
Pierre W <rr21multi@gmail.com>
2024-04-11 00:19:44 +0200

    création de noeuds finis à partir de cartes en grille de l'interface

---
+++ code/carte.py
@@ -0,0 +1,67 @@
+from arrete import Arrete
+from noeud import Noeud, Intersection_T, Intersection_X, EntreeSortie, Virage
+from vecteur_2d import Vecteur2D
+
+class Carte:
+    def __init__(self, largeur:int, hauteur:int, grille_interface=None):
+        self.grille = grille_interface
+        self.largeur = largeur
+        self.hauteur = hauteur
+
+    def into_aretes_noeuds(self) -> (List[Arrete], List[Noeud]):
+        noeuds_dict = {}
+        for xc in range(self.largeur):
+            for yc in range(self.hauteur):
+                if self.est_noeud(xc, yc):
+                    noeuds_dict[(xc, yc)] = []
+        directions = [(-1,0), (1,0), (0,1), (0,-1)]
+        for ((xc, yc), aretes) in noeuds_dict.items():
+            fxc, fyc = xc, yc
+            for (dx, dy) in directions:
+                while self.get_at_or_0(fxc + dx, fyc + dy) == 1:
+                    fxc += dx
+                    fyc += dy
+            if not (fxc == xc and fyc == yc):
+                aretes.append(Arrete(Vecteur2D(xc, yc), Vecteur2D(fxc, fyc)))
+        return [self.cree_noeud(xc, yc, aretes) for ((xc, yc), aretes) in noeuds_dict.items()]
+
+
+    def cree_noeud(self, xc, yc, aretes) -> Noeud:
+        compteur_h = 0
+        compteur_v = 0
+        ret = None
+        decalages = [-1, 1]
+        pos = Vecteur2D(xc, yc)
+        for dx in decalages:
+            compteur_h += self.get_at_or_0(xc + dx, yc)
+        for dy in decalages:
+            compteur_v += self.get_at_or_0(xc, yc + dy)
+        if compteur_v == 1 and compteur_h == 1:
+            ret = Virage(pos, aretes)
+        elif (compteur_h == 1 and compteur_v == 0) or (compteur_h == 0 and compteur_v == 1):
+            ret = EntreeSortie(pos, aretes)
+        elif (compteur_h == 1 and compteur_v == 2) or (compteur_h == 2 and compteur_v == 1):
+            ret = Intersection_T(pos, aretes)
+        else:
+            ret = Intersection_X(pos, aretes)
+        return ret
+    def est_noeud(self, xc, yc) -> bool:
+        compteur_h = 0
+        compteur_v = 0
+        ret = False
+        if self.get_at_or_0(xc, yc) == 1:
+            decalages = [-1, 1]
+            for dx in decalages:
+                compteur_h += self.get_at_or_0(xc + dx, yc)
+            for dy in decalages:
+                compteur_v += self.get_at_or_0(xc, yc + dy)
+            if (compteur_v >= 1 and compteur_h >= 1) or (xc == 0 or xc == self.largeur - 1 or yc == 0 or yc == self.hauteur - 1):
+                ret = True
+        return ret
+
+    def get_at_or_0(self, xc:int, yc:int) -> int:
+        ret = 0
+        if 0 <= xc < self.largeur and 0 <= yc < self.hauteur:
+            ret = self.grille[xc,yc]
+        return ret
+    

6d87ec027357e5840b817fdbac3660a204729b2f
Pyno <pyno.prog@gmail.com>
2024-04-22 15:57:54 +0200

    fix courbe

--- code/courbe.py
+++ code/courbe.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 
@@ -8,28 +9,44 @@ c = 2*np.exp(-1)
 d = 1/2
 
     
-def fonction(x):
+def fonction_e(x):
     return (a*x+b)*np.exp(-np.abs(a*x+b)) / c + d
+
+def fonction_lineaire(x):
+    return x
 
 class Courbe:
 
-    def __init__(self, position_depart: float, position_arrivee: float, vitesse_initiale: float, vitesse_finale: float) -> None:
-        self.position_depart = position_depart
-        self.position_arrivee = position_arrivee
+    def __init__(self,vitesse_initiale: float, vitesse_finale: float, acceleration: float = 8) -> None:
         self.vitesse_initiale = vitesse_initiale
         self.vitesse_finale = vitesse_finale
 
-        self.plage_position = (position_arrivee - position_depart)
+        self.t0 = time.time()
+        self.tf = ((vitesse_finale - vitesse_initiale) / acceleration) + self.t0
+
+
+        self.plage_t = (self.tf - self.t0)
         self.plage_vitesse = (vitesse_finale - vitesse_initiale)
 
         self.active = True
     
-    def result(self, position: float) -> float:
-        position_normalise = (position - self.position_depart) / self.plage_position
-        if position_normalise > 1:
+    def result_e(self, t: float) -> float:
+        temps_normalise = (t - self.t0) / self.plage_t
+        if temps_normalise > 1:
             raise ValueError("position doit être entre position_depart et position_arrivee")
-        vitesse_position_normalise = fonction(position_normalise)
-        vitesse = vitesse_position_normalise * self.plage_vitesse + self.vitesse_initiale
+        vitesse_normalise = fonction_e(temps_normalise)
+        vitesse = vitesse_normalise * self.plage_vitesse + self.vitesse_initiale
+        return vitesse
+    
+
+    def result_lineaire(self, t: float):
+        temps_normalise = (t - self.t0) / self.plage_t
+        if temps_normalise > 1.2:
+            raise ValueError("position doit être entre position_depart et position_arrivee")
+        elif temps_normalise > 1:
+            return self.vitesse_finale
+        vitesse_normalise = fonction_lineaire(temps_normalise)
+        vitesse = vitesse_normalise * self.plage_vitesse + self.vitesse_initiale
         return vitesse
 
     def __eq__(self, courbe) -> bool:
--- code/test2.py
+++ code/test2.py
@@ -1,4 +1,5 @@
 import math
+import time
 from courbe import Courbe
 import numpy as np
 import matplotlib.pyplot as plt
@@ -33,54 +34,53 @@ if __name__ == "__main__":
         # controller les parametre pour voir si la vitesse finale est atteinte
         # faire une courbe avec une acceleration moyenne de 8m/s²
 
-        fps = 60
+        fps = 1/60
 
-        position_depart = 0
-        position_arrivee = 50
+
         vitesse_initiale = 0
         vitesse_finale = 20/3.6
+
+
         
-        courbe = Courbe(position_depart, position_arrivee, vitesse_initiale, vitesse_finale)
-        frames = np.arange(1, 999999999, 1)
+        courbe = Courbe(vitesse_initiale, vitesse_finale, 8)
+        print("courbe.t0", "courbe.tf", "courbe.plage_t", "courbe.plage_vitesse")
+        print(courbe.t0, courbe.tf, courbe.plage_t, courbe.plage_vitesse)
+
+        frames = np.arange(1, 3600, 1)
         
         vitesses = [0]
         positions = [0]
-        temps = [0]
+        temps = [time.time()]
 
 
-
-        acceleration_depart = 0.01
-        position = position_depart
-        vitesse = 0
+        vitesse = vitesse_initiale
 
 
         for frame in frames:
-            temps_courant = frame/fps
+            t = time.time()
             try:
-                if vitesse == 0:
-                    vitesse = (vitesse + acceleration_depart)
-                    position += vitesse/fps
-                else:
-                    vitesse = courbe.result(position)
-                    position += vitesse/fps
+                vitesse = courbe.result_e(t)
+
             except ValueError:
                 print("Erreur")
                 break
-
-            temps.append(temps_courant)
+            
+            print("vitesse", vitesse)
+            print("temps", t)
+            temps.append(t)
             vitesses.append(vitesse)
-            positions.append(position)
 
             """if frame == 5:
                 break"""
-
+            time.sleep(1/60)
 
         #print("temps", temps)
         #print("vitesses", vitesses)
-        #print("positions", positions)
 
+        print(temps)
+        print(vitesses)
         plt.plot(temps, vitesses, label='Vitesse')
-        plt.plot(temps, positions, label='Position')
+        # plt.plot(temps, positions, label='Position')
         plt.xlabel('Temps')
         plt.ylabel('Vitesse / Position')
         plt.title('Évolution de la vitesse et de la position')

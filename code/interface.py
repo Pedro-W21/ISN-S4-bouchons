7253152d88a475bb98b200edf384f27f2767c389
Pierre W <rr21multi@gmail.com>
2024-04-30 17:10:34 +0200

    2eme correction

--- code/interface.py
+++ code/interface.py
@@ -209,9 +209,9 @@ class App(ctk.CTk):
         if self.nom_fichier_valide(nom):
             carte = Carte(self.largeur_carte, self.hauteur_carte, self.grille_route)
             self.assure_existence_dossier_routes()
-            carte.sauvegarder_carte(nom + ".json")
-            self.toplevel.destroy()
             carte.sauvegarder_carte(nom.strip() + ".json")
+            self.toplevel.destroy()
+            
         else:
             print('non valide')
             self.entree_sauvegarde.delete(0, END)
